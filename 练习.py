086a09327efc19d6beca75d93e3ef7e235500934
6666666666-hjv <2338925756@qq.com>
2026-06-16 09:48:52 +0800

    Day 1: 第一行Python代码 + 第2章变量学习

---
+++ 练习.py
@@ -0,0 +1,13 @@
+# message = "hello python"
+# print(message)
+
+# message = "我改变了量"
+# print(message)
+
+name = "ming ming"
+greeting = '你好'
+both = "He said, '你好'"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
